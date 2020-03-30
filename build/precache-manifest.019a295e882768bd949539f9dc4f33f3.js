e550b94e7756c082f1ac6da4f3bdea83d8e4fdf2
Andrew <48319785+isAloneWanderer@users.noreply.github.com>
2020-03-30 14:30:52 +0300

    build folder

---
+++ build/precache-manifest.019a295e882768bd949539f9dc4f33f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204ffcbc6ece0b81ecaffe5c6afa449f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c735e058362887ef8328",
+    "url": "/static/css/main.b39df36b.chunk.css"
+  },
+  {
+    "revision": "09a35465d25ea3450489",
+    "url": "/static/js/2.e67ef955.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e67ef955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c735e058362887ef8328",
+    "url": "/static/js/main.7ff14fb5.chunk.js"
+  },
+  {
+    "revision": "b7ce938e91942ce0a386",
+    "url": "/static/js/runtime-main.7acd6cde.js"
+  }
+]);
